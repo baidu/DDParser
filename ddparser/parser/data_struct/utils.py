5ad7d7947fbb9f5803a60c8e48930d24c517c7ff
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 16:12:05 +0000

    Adding tarfile member sanitization to extractall()

--- ddparser/parser/data_struct/utils.py
+++ ddparser/parser/data_struct/utils.py
@@ -314,7 +314,26 @@ def download_model_from_url(path, model='lstm'):
 
     logging.debug('extacting... to %s' % file_path)
     with tarfile.open(file_path) as tf:
-        tf.extractall(path=temp_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, path=temp_path)
 
     # mv temp_path path
     for _, dirs, _ in os.walk(temp_path):
